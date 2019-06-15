fd12bfb7881952da8275bfc77ebf704ad02ac5ff
vuza <marlon@alagoda.at>
2019-06-15 14:56:09 +0200

    Better example

--- Code_Camp/05_dialogflow_fullfilments/index.js
+++ Code_Camp/05_dialogflow_fullfilments/index.js
@@ -1,5 +1,10 @@
 const ApiBuilder = require('claudia-api-builder')
 const objectPath = require('object-path')
+const aws = require('aws-sdk')
+const DOC = require('dynamodb-doc')
+
+aws.config.update({ region: 'eu-central-1' })
+const docClient = new DOC.DynamoDB()
 
 const api = new ApiBuilder({ mergeVars: true })
 const name = 'fill_parcel_status'
@@ -7,7 +12,7 @@ const name = 'fill_parcel_status'
 api.post(
     '/',
     req =>
-        new Promise(resolve => {
+        new Promise((resolve, reject) => {
             const eventName = objectPath.get(req, 'body.queryResult.queryText')
 
             // Don't handle your own event!
@@ -20,20 +25,46 @@ api.post(
                 'body.queryResult.parameters'
             )
 
+            const parcelId = objectPath.get(parameters, 'parcel_id.parcel_id')
+
             // TODO Do database operations, find status, etc.
             console.log(JSON.stringify(parameters))
-            const status = `a random status ${Math.random()}`
 
-            return resolve({
-                followupEventInput: {
-                    parameters: {
-                        parcel_id: objectPath.get(
-                            parameters,
-                            'parcel_id.parcel_id'
-                        ),
-                        status
-                    },
-                    name
+            const param = {
+                TableName: 'code_camp_parcels',
+                Key: {
+                    parcel_id: parcelId + ''
+                }
+            }
+
+            console.log(JSON.stringify(param))
+
+            docClient.getItem(param, (err, res) => {
+                if (err) {
+                    console.log(err)
+                    return reject(err)
+                }
+
+                if (res.Item.status) {
+                    resolve({
+                        followupEventInput: {
+                            parameters: {
+                                parcel_id: parcelId,
+                                status: res.Item.status
+                            },
+                            name
+                        }
+                    })
+                } else {
+                    resolve({
+                        followupEventInput: {
+                            parameters: {
+                                parcel_id: parcelId,
+                                status: 'Unbekannt'
+                            },
+                            name
+                        }
+                    })
                 }
             })
         })
